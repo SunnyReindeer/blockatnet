201c7ba5770cd1d21d9795596ba39f7aa87b3535
Shibee <a120250369@gmail.com>
2024-01-15 04:34:31 +0800

    11

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -1,8 +1,66 @@
 import { CheckCircleIcon, SettingsIcon } from '@chakra-ui/icons';
-import { Heading, VStack, List, ListIcon, ListItem } from '@chakra-ui/react';
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  Flex,
+  Box,
+} from "@chakra-ui/react";  
+import CryptoPieChart from './CryptoPieChart';
+import PortfolioPerformanceChart from './PortfolioPerformanceChart';
 
 const Home = () => {
+  return (
+    <Flex direction="column">
+      {/* First Row: Portfolio Performance Chart */}
+      <Box flex="1" p={5}>
+        <PortfolioPerformanceChart />
+      </Box>
 
+      {/* Second Row: Other Content */}
+      <Flex>
+        <Box width="20%" p={5} bg="gray.100">
+          {/* Sidebar Content */}
+          <Stat>
+            <StatLabel>Balance</StatLabel>
+            <StatNumber>$123.45</StatNumber>
+            <StatHelpText>As of today</StatHelpText>
+          </Stat> 
+        </Box>
+
+        <Box flex="1" p={5}>
+          {/* Main Content - Placeholder or additional content */}
+        </Box>
+
+        <Box width="20%" p={5} bg="gray.100">
+          {/* Sidebar Content */}
+          <Stat>
+            <StatLabel>Balance</StatLabel>
+            <StatNumber>$123.45</StatNumber>
+            <StatHelpText>As of today</StatHelpText>
+          </Stat> 
+        </Box>
+
+        <Box flex="1" p={5}>
+          {/* Main Content - Placeholder or additional content */}
+        </Box>
+
+        <Box width="20%" p={5} bg="gray.100">
+          {/* Sidebar Content */}
+          <Stat>
+            <StatLabel>Balance</StatLabel>
+            <StatNumber>$123.45</StatNumber>
+            <StatHelpText>As of today</StatHelpText>
+          </Stat> 
+        </Box>
+
+        <Box flex="1" p={5}>
+          <CryptoPieChart />
+        </Box>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
