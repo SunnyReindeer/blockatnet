a48f1d41862b93f95d11c9d94566e56b5566152b
IVFSS <cheunhoyin@gmail.com>
2024-02-05 17:05:21 +0800

    3219

---
+++ blockatnet-main/src/components/elements/index.ts
@@ -0,0 +1,3 @@
+export * from './ColorModeButton';
+export * from './Logo';
+export * from './navigation';
---
+++ blockatnet-main/src/components/layouts/Default/Default.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode } from 'react';
+import { Container } from '@chakra-ui/react';
+import { Footer, Header } from 'components/modules';
+import Head from 'next/head';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Track } from 'components/Track';
+import { Stream } from 'components/Streams';
+
+const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, pageName }) => (
+  <>
+    <Head>
+      <title>{`${pageName} | Blockatnet`}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    
+    <Header />
+    
+
+    <Container centerContent>
+        {children}
+        </Container>
+      <Footer></Footer>
+
+  </>
+);
+
+export default Default;
