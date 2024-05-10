e042cb5a579b0860b295398d212af7383e476003
Shibee <a120250369@gmail.com>
2024-05-10 16:26:32 +0800

    5555

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Box, Link, Text } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const Footer = () => {
-
+<Text>Made by Fintech student</Text>
 };
 
 export default Footer;
