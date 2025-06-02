208f49cc70c4bcff8c4ebe70a5870496ad216acb
Shibee <a120250369@gmail.com>
2025-06-02 17:01:36 +0800

    streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
-st.title("My dApp Embedded in Streamlit")
-st.write("Interact with the dApp below:")
-st.components.v1.iframe("http://localhost:3000", height=800, scrolling=True)
+# 調整 CSS，讓 iframe 更適配視窗，並添加滾動條
+st.markdown("""
+    <style>
+        .main .block-container {
+            padding: 0;
+        }
+        iframe {
+            width: 100vw;
+            height: 100vh;
+            border: none;
+            overflow: auto;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# 調整 iframe 的寬高參數，嘗試更大的寬度
+st.components.v1.iframe("http://localhost:3000", width=5000, height=1080)
