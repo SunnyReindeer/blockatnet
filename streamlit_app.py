1c8d57ca7a8b17230f8f9568533c8d8489ace36a
Shibee <a120250369@gmail.com>
2025-06-02 17:13:27 +0800

    123

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-# 調整 CSS，讓 iframe 更適配視窗，並添加滾動條
+
 st.markdown("""
     <style>
         .main .block-container {
@@ -15,5 +15,5 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# 調整 iframe 的寬高參數，嘗試更大的寬度
+
 st.components.v1.iframe("http://localhost:3000", width=1920, height=1080)
